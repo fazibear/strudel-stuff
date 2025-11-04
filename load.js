e9cfc71d47b70f3210570a2f268ba25b11e277b3
Michał Kalbarczyk <fazibear@gmail.com>
2025-11-04 20:02:33 +0100

    ok

--- load.js
+++ load.js
@@ -1,17 +1,17 @@
 
-let scriptCache = {};
-window.script = async function(url) {
+let loadCache = {};
+window.load = async function(url) {
   if (typeof url !== 'string') {
-    throw new Error('script: expected url string');
+    throw new Error('load: expected url string');
   }
   if (url.startsWith('github:')) {
     const [_, path] = url.split('github:');
     url = 'https://raw.githubusercontent.com/' + path;
   }
-  if (!scriptCache[url]) {
+  if (!loadCache[url]) {
     const response = await fetch(url);
     const text = await response.text();
-    scriptCache[url] = text;
+    loadCache[url] = text;
   }
-  eval(scriptCache[url]);
+  eval(loadCache[url]);
 }
