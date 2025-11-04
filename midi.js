d4f074e5613d40169948e4c401cc4f7932fc0316
Michał Kalbarczyk <fazibear@gmail.com>
2025-11-04 20:27:05 +0100

    gug?

--- midi.js
+++ midi.js
@@ -1,3 +1,3 @@
-window.DRUMS = function(n) { $: n.midichan(1).midi() }
-window.BASS =  function(n) { $: n.midichan(2).midi() }
-window.SYNTH = function(n) { $: n.midichan(3).midi() }
+window.DRUMS = function(n) { return n.midichan(1).midi() }
+window.BASS =  function(n) { return n.midichan(2).midi() }
+window.SYNTH = function(n) { return n.midichan(3).midi() }
