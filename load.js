653e5ca13c77b427dbb5ff65f34263e6d87a5970
Michał Kalbarczyk <fazibear@gmail.com>
2025-11-04 20:30:10 +0100

    make shortcut for me :)

--- load.js
+++ load.js
@@ -3,10 +3,16 @@ window.load = async function(url) {
   if (typeof url !== 'string') {
     throw new Error('load: expected url string');
   }
+
   if (url.startsWith('github:')) {
     const [_, path] = url.split('github:');
     url = 'https://raw.githubusercontent.com/' + path;
+  } else if {
+  if (url.startsWith('fazibear:')) {
+    const [_, path] = url.split('fazibear:');
+    url = 'https://raw.githubusercontent.com/fazibear/strudel-stuff/master/' + path;
   }
+
   if (!window.loadCache[url]) {
     const response = await fetch(url);
     const text = await response.text();
