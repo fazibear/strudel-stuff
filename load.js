fbb4f4079445a60a403396572eb1938c05d2a763
Michał Kalbarczyk <fazibear@gmail.com>
2025-11-11 22:45:38 +0100

    add closures

--- load.js
+++ load.js
@@ -1,31 +1,33 @@
-window.loadCache = window.loadCache || {};
-window.load = async function(url) {
-  if (typeof url !== 'string') {
-    throw new Error('load: expected url string');
-  }
-
-  if (url.startsWith('github:')) {
-    let [_, path] = base.split('github:');
-    path = path.endsWith('/') ? path.slice(0, -1) : path;
-    if (path.split('/').length === 2) {
-      path += '/main';
+(function(){
+  window.loadCache = window.loadCache || {};
+  window.load = async function(url) {
+    if (typeof url !== 'string') {
+      throw new Error('load: expected url string');
     }
 
-    url = 'https://raw.githubusercontent.com/' + path;
-  } else if (url.startsWith('fazibear:')) {
-    const [_, path] = url.split('fazibear:');
-    url = 'https://raw.githubusercontent.com/fazibear/strudel-stuff/main/' + path;
+    if (url.startsWith('github:')) {
+      let [_, path] = base.split('github:');
+      path = path.endsWith('/') ? path.slice(0, -1) : path;
+      if (path.split('/').length === 2) {
+        path += '/main';
+      }
+
+      url = 'https://raw.githubusercontent.com/' + path;
+    } else if (url.startsWith('fazibear:')) {
+      const [_, path] = url.split('fazibear:');
+      url = 'https://raw.githubusercontent.com/fazibear/strudel-stuff/main/' + path;
+    }
+
+    if (!window.loadCache[url]) {
+      logger('[load] fetching: ' + url);
+      const response = await fetch(url);
+      const text = await response.text();
+      window.loadCache[url] = text;
+    }
+    logger('[load] loading: ' + url);
+    eval(window.loadCache[url]);
   }
 
-  if (!window.loadCache[url]) {
-    logger('[load] fetching: ' + url);
-    const response = await fetch(url);
-    const text = await response.text();
-    window.loadCache[url] = text;
-  }
-  logger('[load] loading: ' + url);
-  eval(window.loadCache[url]);
-}
-
-logger('[load] LOADER LOADED');
-logger('[load] Usage: await load(\'github:fazibear/strudel-stuff/ssd.js\') ');
+  logger('[load] LOADER LOADED');
+  logger('[load] Usage: await load(\'github:fazibear/strudel-stuff/ssd.js\') ');
+})();
--- style.js
+++ style.js
@@ -1,16 +1,19 @@
-if(!document.getElementById('fazi-style')){
+(function() {
+  if(!document.getElementById('fazi-style')){
+    var css = `
+      body {
+        scrollbar-color: var(--gutterForeground) var(--gutterBackground)
+        background: black !important;
+      }
+      #code .cm-gutter { background: black !important; }
+    `;
 
-  var css = `
-    body { background: #000000 !important; }
-    body::-webkit-scrollbar { background-color: #00000000; }
-    cm-gutter { background: none !important; }
-  `;
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    style.id = 'fazi-style'
+    style.appendChild(document.createTextNode(css));
 
-  var head = document.head || document.getElementsByTagName('head')[0];
-  var style = document.createElement('style');
-  style.type = 'text/css';
-  style.id = 'fazi-style'
-  style.appendChild(document.createTextNode(css));
-
-  head.appendChild(style);
-}
+    head.appendChild(style);
+  }
+})();
